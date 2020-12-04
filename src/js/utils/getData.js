c3ce995f8523a5e1f52c4a9ba71d866336061041
Hector Lopez <hlopezcarballo@gmail.com>
2020-12-04 03:01:17 -0600

    Makes JSON serving friendly for webpack

--- src/js/utils/getData.js
+++ src/js/utils/getData.js
@@ -1,14 +1,14 @@
-const API = "./js/data.json";
+import Data from "../data.json";
+// const API = "./js/data.json";
 
 const getData = async () => {
   try {
-    const response = await fetch(API);
-    const data = await response.json();
-    return data;
-
-  } catch(error) {
+    const response = await Promise.resolve(Data);
+    // const data = await response.json();
+    return response;
+  } catch (error) {
     console.error("Fecth Error", error.message);
   }
-}
+};
 
 export default getData;
